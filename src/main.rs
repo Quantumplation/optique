b92ba3aa5bb50b3ea5e54a3700cc3ebf6f3fda0f
Pi Lanningham <pi.lanningham@gmail.com>
2021-01-05 02:35:10 -0500

    Read PBRT files from disk
    Didn't feel like implementing all this, so I just found a crate to do it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 #![feature(try_blocks)]
 
 mod options;
+use std::unimplemented;
+
 use clap::Clap;
 use options::*;
 
 fn main() {
     let options: Options = Options::parse();
+
+    let scene = if options.input_files.len() == 1 {
+        let mut scene_info = pbrt_rs::Scene::default();
+        let mut state = pbrt_rs::State::default();
+        pbrt_rs::read_pbrt_file(options.input_files[0].to_str().unwrap(), &mut scene_info, &mut state);
+        (scene_info, state)
+    } else if options.input_files.len() == 0 {
+        unimplemented!("Reading from stdin is currently unimplemented!");
+    } else {
+        unimplemented!("Reading multiple files is currently unimplemented!");
+    };
+    
+    println!("Image size: {:?}", scene.0.image_size);
+    println!("Objects: {}", scene.0.objects.len());
+    println!("Shapes: {}", scene.0.shapes.len());
+    println!("Lights: {}", scene.0.lights.len());
+    println!("Materials: {}", scene.0.materials.len());
+    println!("Instances: {}", scene.0.instances.len());
 }
